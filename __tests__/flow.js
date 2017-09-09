0697bc726f7011683dbb877194fa9c45ef5a1412
Mukesh Soni <mukeshsoni@gmail.com>
2017-09-09 11:39:05 +0530

    added snapshot tests for flow data

--- __tests__/flow.js
+++ __tests__/flow.js
@@ -1,27 +1,27 @@
 import path from 'path'
 import fakeProps from '../src'
 
-// const ComponentFile = path.join(__dirname, '../fixtures/ComponentWithPropTypes.jsx')
+const ComponentFile = path.join(__dirname, '../fixtures/flow/Component.jsx')
 const SimpleFile = path.join(__dirname, '../fixtures/flow/Simple.jsx')
 
 describe('fakeProps', () => {
-  // it('should return an object with required props faked (snapshot)', () => {
-  //   expect(fakeProps(ComponentFile)).toMatchSnapshot()
-  // })
-  //
-  // it('should return an object with all props faked (snapshot)', () => {
-  //   expect(fakeProps(ComponentFile, { optional: true })).toMatchSnapshot()
-  // })
-  //
-  // it('should return more props with optional', () => {
-  //   const allProps = fakeProps(ComponentFile, { optional: true })
-  //   const requiredProps = fakeProps(ComponentFile)
-  //
-  //   const allPropsLength = Object.keys(allProps).length
-  //   const requiredPropsLength = Object.keys(requiredProps).length
-  //
-  //   expect(allPropsLength).toBeGreaterThan(requiredPropsLength)
-  // })
+  it('should return an object with required props faked (snapshot)', () => {
+    expect(fakeProps(ComponentFile)).toMatchSnapshot()
+  })
+
+  it('should return an object with all props faked (snapshot)', () => {
+    expect(fakeProps(ComponentFile, { optional: true })).toMatchSnapshot()
+  })
+
+  it('should return more props with optional', () => {
+    const allProps = fakeProps(ComponentFile, { optional: true })
+    const requiredProps = fakeProps(ComponentFile)
+
+    const allPropsLength = Object.keys(allProps).length
+    const requiredPropsLength = Object.keys(requiredProps).length
+
+    expect(allPropsLength).toBeGreaterThan(requiredPropsLength)
+  })
 
   it('should return an object with shape props faked', () => {
     const props = fakeProps(SimpleFile)
--- fixtures/flow/Component.jsx
+++ fixtures/flow/Component.jsx
@@ -1,2 +1,38 @@
 // TODO: Should use all types defined here in the same order
 // https://github.com/reactjs/react-docgen#types
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+type Address = {
+  street: string,
+  street2?: string,
+  pincode: number
+}
+
+type Repo = {
+  url: string,
+  demoSite?: string,
+  npm?: string
+}
+
+type Person = {
+  id: number,
+  name: string,
+  age: number,
+  address: Address,
+  family?: Array<number>,
+  repos: Array<Repo>
+}
+
+type Props = Person
+
+class MyComponent extends React.Component {
+  props: Props
+
+  render() {
+    // ... do things with the props
+  }
+}
+
+export default MyComponent
