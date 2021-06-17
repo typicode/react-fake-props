bddc675e7c62d20f015cb63ca2b14cb236eaf395
Mauricio Soares <m.soares2603@gmail.com>
2021-06-17 17:04:07 -0300

    feat: Update react-docgen to add typescript support (#20)
    feat: move dependency to devDependency
    
    docs: Update README

---
+++ __tests__/typescript.js
@@ -0,0 +1,69 @@
+import path from 'path'
+import fakeProps from '../src'
+
+const ComponentFile = path.join(
+  __dirname,
+  '../fixtures/typescript/Component.tsx'
+)
+const SimpleFile = path.join(__dirname, '../fixtures/typescript/Simple.tsx')
+const NoProps = require.resolve('../fixtures/typescript/NoProps.tsx')
+const Multiple = require.resolve('../fixtures/typescript/Multiple.tsx')
+
+describe('fakeProps', () => {
+  it('should return an object with no props (snapshot)', () => {
+    expect(fakeProps(NoProps)).toMatchSnapshot()
+  })
+
+  it('should return an object with required props faked (snapshot)', () => {
+    expect(fakeProps(ComponentFile)).toMatchSnapshot()
+  })
+
+  it('should return an object with all props faked (snapshot)', () => {
+    expect(fakeProps(ComponentFile, { optional: true })).toMatchSnapshot()
+  })
+
+  it('should return a person on calling getFriend prop function (test function generation)', () => {
+    const allProps = fakeProps(ComponentFile, { optional: true })
+    const friend = allProps.getFriend()
+
+    expect(friend.name).toBeDefined()
+    expect(friend.name).toBe('getFriend.name')
+    expect(friend.age).toBe(1)
+  })
+
+  it('should return more props with optional', () => {
+    const allProps = fakeProps(ComponentFile, { optional: true })
+    const requiredProps = fakeProps(ComponentFile)
+
+    const allPropsLength = Object.keys(allProps).length
+    const requiredPropsLength = Object.keys(requiredProps).length
+
+    expect(allPropsLength).toBeGreaterThan(requiredPropsLength)
+  })
+
+  it('should return an object with shape props faked', () => {
+    const props = fakeProps(SimpleFile)
+    const { requiredString, requiredObject } = props
+
+    expect(props).toEqual({
+      requiredString,
+      requiredObject: {
+        requiredString: requiredObject.requiredString
+      }
+    })
+  })
+
+  it('should prefix shape props', () => {
+    const props = fakeProps(SimpleFile)
+    expect(props.requiredObject.requiredString).toBe(
+      'requiredObject.requiredString'
+    )
+  })
+
+  it('should work with multiple components', () => {
+    const component = fakeProps(Multiple, { all: true }).find(
+      ({ displayName }) => displayName === 'First'
+    )
+    expect(component.props.requiredString).toBe('requiredString')
+  })
+})
